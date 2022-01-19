c56cc5a96199e3a31f9549cc6da9d572230a673d
woo-ds <swd095611@gmail.com>
2022-01-19 22:44:53 +0900

    백준 문제 풀이
    백준 문제 몇가지 풀었던 코드 집합

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello")
+print("Hello World")
+# 목표가 없으면 할건 딱히 없다.
+# 백준? 유료? 무료OK
+print("강한친구 대한육군")  
+print("강한친구 대한육군")  
+print("\    /\ ")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
+a = input(1 + 2)
+print(a)
